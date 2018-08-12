6c80f1c615ead7bfa8d0c256458a7c025f8f4285
patzjo <jonne.patzold@gmail.com>
2018-08-12 05:42:57 +0300

    Update.

--- ItsAGame/deploy/src/Assets.cpp
+++
@@ -1,8 +0,0 @@
-#include "Assets.h"
-
-const char *assetsDirectory = "Assets/";
-const char *fontsDirectory = "Fonts/";
-const char *texturesDirectory = "Textures/";
-const char *soundsDirectory = "Sounds/";
-const char *musicsDirectory = "Musics/";
-const char *stencilsDirectory = "Stencils/";
